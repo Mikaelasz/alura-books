d74946d938865d0ab41261dcf6827e01d907b78d
mikaela <mikaela.araujo@dnkinfotelecom.com.br>
2023-09-11 11:48:27 -0300

    Card reutilizável com props

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,6 +1,8 @@
 import { livros } from './dadosUltimosLancamentos'
 import styled from 'styled-components'
 import { Titulo } from '../Titulo'
+import CardRecomenda from '../CardRecomenda'
+import imagemLivro from '../../imagens/livro2.png'
 
 const UltimosLancamentosContainer = styled.section`
     background-color: #EBECEE;
@@ -21,15 +23,24 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
     return (
         <UltimosLancamentosContainer>
-            <Titulo 
-                cor="#EB9B00" 
-                tamanhoFonte="36px"              
-            >ÚLTIMOS LANÇAMENTOS</Titulo>
+            <Titulo
+                cor="#EB9B00"
+                tamanhoFonte="36px"
+            >
+                ÚLTIMOS LANÇAMENTOS
+            </Titulo>
             <NovosLivrosContainer>
-            {livros.map(livro => (
-                <img src={livro.src} alt="" />
-            ))}
+                {livros.map(livro => (
+                    <img src={livro.src} alt="" />
+                ))}
             </NovosLivrosContainer>
+            <CardRecomenda 
+                titulo='Talvez você se interesse por ...'
+                subtitulo='Angular 11'
+                descricao='Construindo uma aplicação com a plataforma Google'
+                img ={imagemLivro}
+            />           
+
         </UltimosLancamentosContainer>
 
 
